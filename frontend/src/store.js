f04dac52d920500c45846141161e7be077e1673d
krishapatel14 <krishapatel1411@gmail.com>
2024-02-17 17:10:53 +0530

    login

--- frontend/src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import bankReducer from "./redux/BankSlice"
-
-export const store = configureStore({
-    reducer:{
-        bank:bankReducer
-    }
-})
