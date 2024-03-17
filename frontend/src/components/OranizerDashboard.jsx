83a4b3fdbf04fd7ab2c097bddfa16beef81d3c77
krishapatel14 <krishapatel1411@gmail.com>
2024-03-17 17:51:07 +0530

    organizer

--- frontend/src/components/OranizerDashboard.jsx
+++ frontend/src/components/OranizerDashboard.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
 export const OranizerDashboard = () => {
+  const divStyle1 = {
+    color: 'blue',
+    padding: '10px',
+    margin:'20px'
+  };
   return (
-    <div>OranizerDashboard</div>
+    <div>
+      <div style={divStyle1}><button>Add Event</button></div>
+      <div style={divStyle1}><button>Update Event</button></div>
+      <div style={divStyle1}><button>Delete Event</button></div>
+      <div style={divStyle1}><button>Display Event Details</button></div>
+      <div style={divStyle1}><button>Past Events history</button></div>
+    </div>
   )
 }
