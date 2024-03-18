ae39ab5fd1510f7b674acc84694e9270b88587b5
krishapatel14 <krishapatel1411@gmail.com>
2024-03-18 10:21:15 +0530

    org

--- frontend/src/components/OranizerDashboard.jsx
+++ frontend/src/components/OranizerDashboard.jsx
@@ -1,18 +1,51 @@
 import React from 'react'
 
 export const OranizerDashboard = () => {
-  const divStyle1 = {
-    color: 'blue',
-    padding: '10px',
-    margin:'20px'
-  };
+  const nav = {
+    border: "2px solid black",
+    padding: "20px",
+    "background-color": "cadetblue"
+  }
+  const nav1={
+    "padding-right":"750px",
+    "font-size":"larger",
+    "font-family": "'Trebuchet MS', 'Lucida Grande', 'Arial', sans-serif"
+  }
+  const nav2={
+    "padding-left": "30px"
+  }
+  const crud_main={
+    // border:"2px solid blue",
+    padding:"275px 10px 275px 125px"
+  }
+  const crud={
+    border:"2px solid blue",
+    "border-radius":"10px",
+    "border-radius": "10px",
+    "background-color": "rgb(5, 5, 38)",
+    padding:"150px 70px 150px 70px",
+    color: "aliceblue",
+    opacity: "0.9",
+    "margin-right": "80px"
+
+  }
   return (
-    <div>
-      <div style={divStyle1}><button>Add Event</button></div>
-      <div style={divStyle1}><button>Update Event</button></div>
-      <div style={divStyle1}><button>Delete Event</button></div>
-      <div style={divStyle1}><button>Display Event Details</button></div>
-      <div style={divStyle1}><button>Past Events history</button></div>
-    </div>
+    <>
+      <nav style={nav}>
+        <span id="nav1" style={nav1}>GradEvents</span>
+        <span id="nav2" style={nav2}>EventList</span>
+        <span id="nav3"  style={nav2}/>
+        History
+        <span id="nav4" style={nav2}>Profile</span>
+      </nav>
+      <div id="crud1" style={crud_main}>
+        <span id="create" style={crud}>Create Event</span>
+        <span id="update" style={crud}>Update Event</span>
+        <span id="review" style={crud}>Review Event</span>
+        <span id="delete" style={crud}>Delete Event</span>
+        &nbsp;&nbsp;
+      </div>
+    </>
+
   )
 }
